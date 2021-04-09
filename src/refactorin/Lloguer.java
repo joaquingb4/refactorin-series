093e69e847bc59729e7fa6d1b7b2d6184d44ed30
joaquingb4 <joaquin_alonsogarcia@iesjoandaustria.org>
2021-04-09 19:57:22 +0200

    ¡Primera versión!

---
+++ src/refactorin/Lloguer.java
@@ -0,0 +1,10 @@
+package refactorin;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Lloguer {
+	private Date data;
+	private int dies;
+	private ArrayList <Vehicle> vehicles;
+}
---
+++ src/refactorin/Vehicle.java
@@ -0,0 +1,16 @@
+package refactorin;
+
+import java.util.ArrayList;
+
+public class Vehicle {
+	private String model;
+	private String marca;
+	private String categoria;
+	
+	public Vehicle (String model, String marca, String categoria) {
+		this.model = model;
+		this.marca = marca;
+		this.categoria = categoria;
+	}
+	public static final String BASIC = null;
+}
